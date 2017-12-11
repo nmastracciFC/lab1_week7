d943eb63763c357b84cdd91eb08bb705e2fe67fe
nmastracci <mastracci.natalie@gmail.com>
2017-12-11 17:48:18 -0500

    video thumb inital parts functional and returning json object

--- includes/functions.php
+++ includes/functions.php
@@ -42,7 +42,19 @@ if (isset($_GET['carModel'])) {
 
 }
 
+if (isset($_GET['getVideos'])) {
 
+	$myQuery = "SELECT * FROM video";
+
+	$result = mysqli_query($conn, $myQuery);
+	$rows = array();
+
+	while($row = mysqli_fetch_assoc($result)) {
+		$rows[] = $row;
+	}
+
+	echo json_encode($rows);
+}
 
 
 
