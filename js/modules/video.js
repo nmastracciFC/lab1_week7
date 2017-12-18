1ee24b83a29a1b5135f10684dd6976cead8d1cfd
nmastracci <mastracci.natalie@gmail.com>
2017-12-18 16:15:19 -0500

    added thumbnails and video switching functionality

--- js/modules/video.js
+++ js/modules/video.js
@@ -9,14 +9,14 @@ var video = {
 		//have to start at the root object (namespace) name
 		video.videoPlayer.muted = false;
 		video.volumeIndicator.classList.replace('fa-volume-off', 'fa-volume-up');
-		console.log("on");
+		// console.log("on");
 	},
 
 	volOff() {
 		//have to start at the root object (namespace) name
 		video.videoPlayer.muted = true;
 		video.volumeIndicator.classList.replace('fa-volume-up', 'fa-volume-off');
-		console.log("off");
+		// console.log("off");
 	},
 
 	popOverlay() {
@@ -44,10 +44,40 @@ var video = {
 				console.log(error);
 			});
 
+		let overlay = document.querySelector('.vid-overlay');
+		overlay.classList.remove('show-overlay');
+
+		video.volOn();
+
 	},
 
 	loadVideoThumbs(data) {
-		debugger;
+		// debugger;
+		let thumbHolder = document.querySelector('.video-thumbs');
+
+		data.forEach(thumb => {
+			//JS template string needs the back tick
+			let docFrag = `<li class="vid-thumb" role="button" data-videopath="${thumb.path}">
+          					<img src="images/${thumb.placeholder}" alt="mini commercial" class="responsive">
+        					</li>`;
+        	//data-videopath is the field that has the database file name?
+
+        	thumbHolder.innerHTML += docFrag;
+        	//+= is an assignment operator
+		})
+		//orange coloured 'thumb' refers to current list element
+		thumbHolder.querySelectorAll('li').forEach((thumb) => thumb.addEventListener('click', video.loadNewVideo));
+
+	},
+
+	loadNewVideo() {
+		// debugger;
+		let videoPath = "video/" + this.dataset.videopath;
+		//every list item has an attribute called videopath so you collect them all set the source
+
+		video.videoPlayer.src = videoPath;
+		video.videoPlayer.load();
+		video.videoPlayer.play();
 
 	},
 
